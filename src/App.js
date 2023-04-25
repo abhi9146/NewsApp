437381a6e25d56a46d3ed27f8188282ab0e763ba
abhishek <abhisunkarwad2002@gmail.com>
2023-04-25 19:45:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+
+export default function App() {
+  return (
+    <>
+    <Navbar />
+    <News />
+    </>
+  )
+}
